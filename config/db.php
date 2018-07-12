7026085d7c0730b22b6bef360889d16fd0bd6a83
serega22111991 <serega22111991@gmail.com>
2018-07-12 03:48:05 +0300

    make table and autorization v1.1

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=smsclub',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+]
+
+?>
---
+++ models/Users.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $username
+ * @property string $password
+ * @property string $auth_key
+ * @property string $access_token
+ * @property string $avatar
+ * @property string $role
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'username', 'avatar', 'role'], 'required'],
+            [['name', 'username', 'password', 'auth_key', 'access_token', 'avatar', 'role'], 'string', 'max' => 32],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'username' => 'Username',
+            'password' => 'Password',
+            'auth_key' => 'Auth Key',
+            'access_token' => 'Access Token',
+            'avatar' => 'Avatar',
+            'role' => 'Role',
+        ];
+    }
+}
+?>
